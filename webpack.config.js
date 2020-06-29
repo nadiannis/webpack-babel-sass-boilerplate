3bd2509f15c45f01b2467ef8f2ae8f137ea4ee64
nadiannis <annisanadianeyla@gmail.com>
2020-06-29 22:16:18 +0700

    Modify webpack file to make use of the clean plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/scripts/index.js',
@@ -11,7 +12,8 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[id].css'
-    })
+    }),
+    new CleanWebpackPlugin()
   ],
   module: {
     rules: [
